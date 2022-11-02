a903b80aeca00aee145b4eb8432b1a15946cd494
walemust <walemust28@gmail.com>
2022-11-02 18:16:43 +0100

    added server to help run the code

---
+++ server.js
@@ -0,0 +1,6 @@
+const http = require('http')
+const app = require('./index')
+const { PORT } = require('./access/access')
+
+const server = http.createServer(app)
+server.listen(PORT, () => console.log(`Running in ${process.env.NODE_ENV} mode on port ${PORT}`))
