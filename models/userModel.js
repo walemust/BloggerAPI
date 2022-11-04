972bbacf76584f8b0e4fd50c633b281e6f7e463c
walemust <walemust28@gmail.com>
2022-11-04 23:40:04 +0100

    corrected the models schema

--- models/userModel.js
+++ models/userModel.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const userModel = new mongoose.Schema({
-  first_name: {
+  firstName: {
     type: String,
     required: true,
   },
-  last_name: {
+  lastName: {
     type: String,
     required: true,
   },
