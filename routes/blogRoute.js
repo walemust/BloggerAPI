7273270836916160a5f8410a86f62f32f801d6de
walemust <walemust28@gmail.com>
2022-11-06 23:12:47 +0100

    added blogcontrol

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,22 +1,20 @@
 const router = require("express").Router();
-const { createBlog, getListOfPublishedBlogs, getPublishedBlog, updatePublishedBlog,
-    deletePublishedBlog,
-    updatePublishedBlogState } = require("../controllers/blogs");
+const blogControl = require("../controllers/blogs");
 const { filterAndSort, filterByPublished, list, setUserFilter } = require('../middleware/apiPoints')
 const getBearerToken = require("../middleware/bearerToken");
 const { getUserFromToken, assignUser } = require("../middleware/validateUser");
 const pagination = require("../middleware/pagination");
 const creator = require('../middleware/creator')
 
-router.route('/').get(filterAndSort, filterByPublished, list, pagination, getListOfPublishedBlogs)
-    .post(getUserFromToken, createBlog)
+router.route('/').get(filterAndSort, filterByPublished, list, pagination, blogControl.getListOfPublishedBlogs)
+    .post(getUserFromToken, blogControl.createBlog)
 
-router.route("/g").get(getUserFromToken, filterAndSort, setUserFilter, pagination, getListOfPublishedBlogs)
+router.route("/g").get(getUserFromToken, filterAndSort, setUserFilter, pagination, blogControl.getListOfPublishedBlogs)
 
-router.route('/:id').get(assignUser, getPublishedBlog)
-    .patch(getUserFromToken, creator, updatePublishedBlogState)
-    .put(getUserFromToken, creator, updatePublishedBlog)
-    .delete(getUserFromToken, creator, deletePublishedBlog)
+router.route('/:id').get(assignUser, blogControl.getPublishedBlog)
+    .patch(getUserFromToken, creator, blogControl.updatePublishedBlogState)
+    .put(getUserFromToken, creator, blogControl.updatePublishedBlog)
+    .delete(getUserFromToken, creator, blogControl.deletePublishedBlog)
 
 
 module.exports = router;
