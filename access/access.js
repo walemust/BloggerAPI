33f92415b7b5b1cf9b32cb8ebb9ac8d0cf43fef9
walemust <walemust28@gmail.com>
2022-11-06 07:08:13 +0100

    checking for connectivity

--- access/access.js
+++ access/access.js
@@ -1,8 +1,8 @@
-require("dotenv").config({ path: "../.env" });
+require("dotenv").config();
 const MONGO_URI =
   process.env.NODE_ENV === "test"
-    ? process.env.TEST_MONGO_URL
-    : process.env.MONGO_URL;
+    ? process.env.TEST_MONGO_URI
+    : process.env.MONGO_URI;
 const PORT = process.env.PORT;
 const SECRET = process.env.SECRET;
 
